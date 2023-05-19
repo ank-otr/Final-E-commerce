30fb6a26234ef3e261d616c71d9d21eac8c396d1
Ankit <ank.kumar077@gmail.com>
2023-05-19 14:45:10 +0545

    Update

---
+++ JS/cv.js
@@ -0,0 +1,47 @@
+// Get the button element
+const cvButton = document.getElementById("cvButton");
+
+// Add click event listener to the button
+cvButton.addEventListener("click", function() {
+  // Retrieve the PDF path from the data attribute
+  const pdfPath = cvButton.getAttribute("data-pdf");
+  
+  // Open the PDF CV in a new tab or window
+  window.open(pdfPath, "_blank");
+});
+
+// Get the button element
+const cvButton2 = document.getElementById("cvButton3");
+
+// Add click event listener to the button
+cvButton2.addEventListener("click", function() {
+  // Retrieve the PDF path from the data attribute
+  const pdfPath = cvButton2.getAttribute("data-pdf");
+  
+  // Open the PDF CV in a new tab or window
+  window.open(pdfPath, "_blank");
+});
+
+// Get the button element
+const cvButton3 = document.getElementById("cvButton3");
+
+// Add click event listener to the button
+cvButton3.addEventListener("click", function() {
+  // Retrieve the PDF path from the data attribute
+  const pdfPath = cvButton3.getAttribute("data-pdf");
+  
+  // Open the PDF CV in a new tab or window
+  window.open(pdfPath, "_blank");
+});
+
+// Get the button element
+const cvButton4 = document.getElementById("cvButton3");
+
+// Add click event listener to the button
+cvButton4.addEventListener("click", function() {
+  // Retrieve the PDF path from the data attribute
+  const pdfPath = cvButton4.getAttribute("data-pdf");
+  
+  // Open the PDF CV in a new tab or window
+  window.open(pdfPath, "_blank");
+});
